2ddcb48e6668aaa22e2ba04c1fffbab4cc5442f5
Leonel <srleo360@gmail.com>
2023-06-14 15:23:11 -0300

    Se modifico el archivo html con el link de JS dentro del body y el archivo Js se le agregó el funcionamiento la calculadora

---
+++ HTML/mains.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pantalla = document.querySelector('.pantalla');
+    const calculadora = document.querySelector('.calculadora');
+    const botones = document.querySelectorAll('button[name="tecla"], button[name="operador"], button[name="igual"], button[name="borrar"], button[name="borrar1"]');
+  
+    let operacion = '';
+    let resultado = '';
+  
+    botones.forEach((button) => {
+      button.addEventListener('click', () => {
+        const tecla = button.textContent;
+  
+        if(tecla === 'C') {
+           limpiarPantalla();
+        } else if (tecla === '=') {
+          calcularResultado();
+        } else if (tecla === '<') {
+          eliminarUltimoCaracter();
+        } else {
+          agregarCaracter(tecla);
+        }
+      });
+    });
+  
+    function agregarCaracter(caracter) {
+      operacion += caracter;
+      actualizarPantalla(operacion);
+    }
+  
+    function eliminarUltimoCaracter() {
+      operacion = operacion.slice(0, -1);
+      actualizarPantalla(operacion);
+    }
+  
+    function calcularResultado() {
+      try {
+        resultado = eval(operacion);
+        actualizarPantalla(resultado);
+      } catch (error) {
+        actualizarPantalla('Error');
+      }
+    }
+  
+    function limpiarPantalla() {
+      operacion = '';
+      resultado = '';
+      actualizarPantalla('');
+    }
+  
+    function actualizarPantalla(valor) {
+      pantalla.textContent = valor;
+    }
+  });
